907309e1cd5efb717b72a5c0b0a948d06160889e
Aishwarya <p.aishwarya50@gmail.com>
2022-11-24 00:41:26 -0500

    image changes aishwarya

--- tasks/seed.js
+++ tasks/seed.js
@@ -24,8 +24,8 @@ async function main() {
 
     // Puzzle
     //pending approval
-    const H1 = await Puzzle.addPuzzle("Tetris", "Tetris", ["TET"], "https://tetris.com/play-tetris", ["../../../public/images/tetris.jpg"]);
-    const H2 = await Puzzle.addPuzzle("The Talos Principle", "The Talos Principle", ["TET"], "https://store.steampowered.com/app/257510/The_Talos_Principle/", ["../../../public/images/TheTalosPrinciple.jpg"]);
+    const H1 = await Puzzle.addPuzzle("The Talos Principle", "The Talos Principle", ["TET"], "https://store.steampowered.com/app/257510/The_Talos_Principle/", ["../../../public/images/TheTalosPrinciple.gif"]);
+    const H2 = await Puzzle.addPuzzle("Tetris", "Tetris", ["TET"], "https://tetris.com/play-tetris", ["../../../public/images/tetris.jpg"]);
     const H3 = await Puzzle.addPuzzle("Portals 2", "Portals 2", ["TET"], "https://store.steampowered.com/app/620/Portal_2/", ["../../../public/images/Portals2.jpg"]);
     const H4 = await Puzzle.addPuzzle("Baba Is You", "Baba Is You", ["TET"], "https://store.steampowered.com/app/736260/Baba_Is_You/", ["../../../public/images/BabaIsYou.jpg"]);
     const H5 = await Puzzle.addPuzzle("Fez", "Fez", ["TET"], "https://store.steampowered.com/app/224760/FEZ/", ["../../../public/images/Fez.jpg"]);
@@ -33,35 +33,35 @@ async function main() {
 
     //action
     const A1 = await Action.addAction("Tomb Raider", "Tomb Raider", ["TET"], "https://store.steampowered.com/agecheck/app/203160/", ["../../../public/images/TombRaider.jpg"]);
-    const A2 = await Action.addAction("Resident Evil 4", "Resident Evil 4", ["TET"], "https://www.residentevil.com/4/", ["../../../public/images/TheTalosPrinciple.jpg"]);
-    const A3 = await Action.addAction("Marvel’s Guardians of Galaxys", "Marvel’s Guardians of Galaxys", ["TET"], "https://store.steampowered.com/app/1088850/Marvels_Guardians_of_the_Galaxy/", ["../../../public/images/Portals2.jpg"]);
-    const A4 = await Action.addAction("Assassins Creed Valhalla", "Assassins Creed Valhalla", ["TET"], "https://store.steampowered.com/app/2208920/Assassins_Creed_Valhalla/", ["../../../public/images/BabaIsYou.jpg"]);
-    const A5 = await Action.addAction("Batman: Arkham Asylums", "Batman: Arkham Asylums", ["TET"], "https://store.steampowered.com/app/35140/Batman_Arkham_Asylum_Game_of_the_Year_Edition/", ["../../../public/images/Fez.jpg"]);
-    const A6 = await Action.addAction("Psychopaths 2", "Psychopaths 2", ["TET"], "https://store.steampowered.com/app/607080/Psychonauts_2/", ["../../../public/images/Bejeweled3.jpg"]);
+    const A2 = await Action.addAction("Batman: Arkham Asylums", "Batman: Arkham Asylums", ["TET"], "https://store.steampowered.com/app/35140/Batman_Arkham_Asylum_Game_of_the_Year_Edition/", ["../../../public/images/Batman.gif"]);
+    const A3 = await Action.addAction("Marvel’s Guardians of Galaxys", "Marvel’s Guardians of Galaxys", ["TET"], "https://store.steampowered.com/app/1088850/Marvels_Guardians_of_the_Galaxy/", ["../../../public/images/Guardians.gif"]);
+    const A4 = await Action.addAction("Assassins Creed Valhalla", "Assassins Creed Valhalla", ["TET"], "https://store.steampowered.com/app/2208920/Assassins_Creed_Valhalla/", ["../../../public/images/Assassins_Creed_Valhalla.gif"]);
+    const A5 = await Action.addAction("Resident Evil 4", "Resident Evil 4", ["TET"], "https://www.residentevil.com/4/", ["../../../public/images/ResidentEvil4.gif"]);
+    const A6 = await Action.addAction("Uncharted 4 : A Thief’s End", "Uncharted 4 : A Thief’s End", ["TET"], "https://store.steampowered.com/app/607080/Psychonauts_2/", ["../../../public/images/Uncharted 4.gif"]);
 
     //racing
-    const R1 = await Racing.addRacing("Tomb Raider", "Tomb Raider", ["TET"], "https://store.steampowered.com/agecheck/app/203160/", ["../../../public/images/TombRaider.jpg"]);
-    const R2 = await Racing.addRacing("Resident Evil 4", "Resident Evil 4", ["TET"], "https://www.residentevil.com/4/", ["../../../public/images/TheTalosPrinciple.jpg"]);
-    const R3 = await Racing.addRacing("Marvel’s Guardians of Galaxys", "Marvel’s Guardians of Galaxys", ["TET"], "https://store.steampowered.com/app/1088850/Marvels_Guardians_of_the_Galaxy/", ["../../../public/images/Portals2.jpg"]);
-    const R4 = await Racing.addRacing("Assassins Creed Valhalla", "Assassins Creed Valhalla", ["TET"], "https://store.steampowered.com/app/2208920/Assassins_Creed_Valhalla/", ["../../../public/images/BabaIsYou.jpg"]);
-    const R5 = await Racing.addRacing("Batman: Arkham Asylums", "Batman: Arkham Asylums", ["TET"], "https://store.steampowered.com/app/35140/Batman_Arkham_Asylum_Game_of_the_Year_Edition/", ["../../../public/images/Fez.jpg"]);
-    const R6 = await Racing.addRacing("Psychopaths 2", "Psychopaths 2", ["TET"], "https://store.steampowered.com/app/607080/Psychonauts_2/", ["../../../public/images/Bejeweled3.jpg"]);
+    const R1 = await Racing.addRacing("Dirt Rally 2.0", "Dirt Rally 2.0", ["TET"], "https://store.steampowered.com/agecheck/app/203160/", ["../../../public/images/DirtRally.gif"]);
+    const R2 = await Racing.addRacing("Forza Horizon 5", "Forza Horizon 5", ["TET"], "https://www.residentevil.com/4/", ["../../../public/images/ForzaHorizon5.gif"]);
+    const R3 = await Racing.addRacing("Need for Speed: Hot Pursuit", "Need for Speed: Hot Pursuit", ["TET"], "https://store.steampowered.com/app/1088850/Marvels_Guardians_of_the_Galaxy/", ["../../../public/images/NFS.gif"]);
+    const R4 = await Racing.addRacing("Project CARS 2", "Project CARS 2", ["TET"], "https://store.steampowered.com/app/2208920/Assassins_Creed_Valhalla/", ["../../../public/images/ProjectCARS2.gif"]);
+    const R5 = await Racing.addRacing("F1 2020", "F1 2020", ["TET"], "https://store.steampowered.com/app/35140/Batman_Arkham_Asylum_Game_of_the_Year_Edition/", ["../../../public/images/F12020.gif"]);
+    const R6 = await Racing.addRacing("Forza Motorsport 7", "Forza Motorsport 7", ["TET"], "https://store.steampowered.com/app/607080/Psychonauts_2/", ["../../../public/images/Forza.gif"]);
 
     //sports
-    const S1 = await Sports.addSports("Wii Sports", "Wii Sports", ["TET"], "https://store.steampowered.com/agecheck/app/203160/", ["../../../public/images/TombRaider.jpg"]);
-    const S2 = await Sports.addSports("ESPN NFL 2K5", "ESPN NFL 2K5", ["TET"], "https://www.residentevil.com/4/", ["../../../public/images/TheTalosPrinciple.jpg"]);
-    const S3 = await Sports.addSports("MARIO TENNIS", "MARIO TENNIS", ["TET"], "https://store.steampowered.com/app/1088850/Marvels_Guardians_of_the_Galaxy/", ["../../../public/images/Portals2.jpg"]);
-    const S4 = await Sports.addSports("NFL STREET", "NFL STREET", ["TET"], "https://store.steampowered.com/app/2208920/Assassins_Creed_Valhalla/", ["../../../public/images/BabaIsYou.jpg"]);
-    const S5 = await Sports.addSports("Nintendo switch sports", "Nintendo switch sports", ["TET"], "https://store.steampowered.com/app/35140/Batman_Arkham_Asylum_Game_of_the_Year_Edition/", ["../../../public/images/Fez.jpg"]);
-    const S6 = await Sports.addSports("Mario Golf", "Mario Golf", ["TET"], "https://store.steampowered.com/app/607080/Psychonauts_2/", ["../../../public/images/Bejeweled3.jpg"]);
+    const S1 = await Sports.addSports("MARIO TENNIS", "MARIO TENNIS", ["TET"], "https://store.steampowered.com/app/1088850/Marvels_Guardians_of_the_Galaxy/", ["../../../public/images/Mario_Tennis.gif"]);
+    const S2 = await Sports.addSports("Fifa 10", "Fifa 10", ["TET"], "https://www.residentevil.com/4/", ["../../../public/images/Fifa_10.gif"]);
+    const S3 = await Sports.addSports("Wii Sports", "Wii Sports", ["TET"], "https://store.steampowered.com/agecheck/app/203160/", ["../../../public/images/wii.gif"]);
+    const S4 = await Sports.addSports("NFL STREET", "NFL STREET", ["TET"], "https://store.steampowered.com/app/2208920/Assassins_Creed_Valhalla/", ["../../../public/images/nfl.gif"]);
+    const S5 = await Sports.addSports("Nintendo switch sports", "Nintendo switch sports", ["TET"], "https://store.steampowered.com/app/35140/Batman_Arkham_Asylum_Game_of_the_Year_Edition/", ["../../../public/images/Nintendo.gif"]);
+    const S6 = await Sports.addSports("Mario Golf", "Mario Golf", ["TET"], "https://store.steampowered.com/app/607080/Psychonauts_2/", ["../../../public/images/Mario_golf.gif"]);
 
     //strategy
-    const ST1 = await Strategy.addStrategy("Supremacy 1914.", "Supremacy 1914.", ["TET"],"https://store.steampowered.com/agecheck/app/203160/",["../../../public/images/TombRaider.jpg"]);
-    const ST2 = await Strategy.addStrategy("Crusader Kings 3", "Crusader Kings 3", ["TET"],"https://www.residentevil.com/4/",["../../../public/images/TheTalosPrinciple.jpg"]);
-    const ST3 = await Strategy.addStrategy("Offworld Trading Company", "Offworld Trading Company", ["TET"],"https://store.steampowered.com/app/1088850/Marvels_Guardians_of_the_Galaxy/",["../../../public/images/Portals2.jpg"]);
-    const ST4 = await Strategy.addStrategy("XCOM 2", "XCOM 2", ["TET"],"https://store.steampowered.com/app/2208920/Assassins_Creed_Valhalla/",["../../../public/images/BabaIsYou.jpg"]);
-    const ST5 = await Strategy.addStrategy("Homeworld: Deserts of Kharak", "Homeworld: Deserts of Kharak", ["TET"],"https://store.steampowered.com/app/35140/Batman_Arkham_Asylum_Game_of_the_Year_Edition/",["../../../public/images/Fez.jpg"]);
-    const ST6 = await Strategy.addStrategy("Civilization 6", "Civilization 6", ["TET"],"https://store.steampowered.com/app/607080/Psychonauts_2/",["../../../public/images/Bejeweled3.jpg"]);
+    const ST1 = await Strategy.addStrategy("Offworld Trading Company", "Offworld Trading Company", ["TET"],"https://store.steampowered.com/app/1088850/Marvels_Guardians_of_the_Galaxy/",["../../../public/images/offworld.gif"]);
+    const ST2 = await Strategy.addStrategy("Crusader Kings 3", "Crusader Kings 3", ["TET"],"https://www.residentevil.com/4/",["../../../public/images/crusader.gif"]);
+    const ST3 = await Strategy.addStrategy("Supremacy 1914", "Supremacy 1914", ["TET"],"https://store.steampowered.com/agecheck/app/203160/",["../../../public/images/supremacy.gif"]);
+    const ST4 = await Strategy.addStrategy("XCOM 2", "XCOM 2", ["TET"],"https://store.steampowered.com/app/2208920/Assassins_Creed_Valhalla/",["../../../public/images/xcom2.gif"]);
+    const ST5 = await Strategy.addStrategy("Homeworld: Deserts of Kharak", "Homeworld: Deserts of Kharak", ["TET"],"https://store.steampowered.com/app/35140/Batman_Arkham_Asylum_Game_of_the_Year_Edition/",["../../../public/images/homeworld.gif"]);
+    const ST6 = await Strategy.addStrategy("Civilization 6", "Civilization 6", ["TET"],"https://store.steampowered.com/app/607080/Psychonauts_2/",["../../../public/images/civilization6.gif"]);
 
     // data= await Puzzle.getAllPuzzles();
     // H1_id= data[data.length - 1]._id;
